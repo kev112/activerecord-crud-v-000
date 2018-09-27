dad886a47f7f166ef8802b374fdb667cafeb5e28
Tao Liu <taoliudesign@gmail.com>
2018-09-27 02:49:53 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -71,8 +71,6 @@ end
 def can_update_using_update_method
   # Update movie title to "Wat, huh?"
   Movie.create(title: "Wat?").update title: "Wat?"
-  __
-  __
 end
 
 def can_update_multiple_items_at_once
