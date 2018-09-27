a97f9efa6ea3425cfd3c442a8d8c7024890f8ab1
Tao Liu <taoliudesign@gmail.com>
2018-09-27 03:01:23 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -67,7 +67,7 @@ def can_be_found_updated_and_saved
   Movie.create(title: "Awesome Flick")
   m = Movie.find_by title: "Awesome Flick"
   m.update title: "Even Awesomer Flick"
-  binding.pry
+  # binding.pry
 end
 
 def can_update_using_update_method
